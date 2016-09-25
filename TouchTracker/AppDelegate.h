94232b626ff709498bda9798cc37364917205f22
abwhitley <a.blakewhitley@gmail.com>
2016-09-24 22:23:47 -0400

    Initial Commit

---
+++ TouchTracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchTracker
+//
+//  Created by Austins Work on 9/24/16.
+//  Copyright © 2016 AustinsIronYard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
